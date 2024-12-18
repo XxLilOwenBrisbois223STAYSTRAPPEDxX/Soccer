885e46ecf561a482d84239ed73afbd0ff12be51c
owenbris825 <owenbris825@SDS-L-9982Y93>
2024-12-18 11:00:00 -0500

    Add project files.

---
+++ Soccer/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Soccer
+{
+    partial class Soccer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Soccer));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.endLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.AutoSize = true;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(296, 29);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(14, 16);
+            this.p2ScoreLabel.TabIndex = 0;
+            this.p2ScoreLabel.Text = "0";
+            this.p2ScoreLabel.Click += new System.EventHandler(this.p2ScoreLabel_Click);
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.AutoSize = true;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(176, 29);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(14, 16);
+            this.p1ScoreLabel.TabIndex = 1;
+            this.p1ScoreLabel.Text = "0";
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.BackColor = System.Drawing.Color.Transparent;
+            this.endLabel.Location = new System.Drawing.Point(12, 29);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(10, 16);
+            this.endLabel.TabIndex = 2;
+            this.endLabel.Text = " ";
+            // 
+            // Soccer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(495, 344);
+            this.Controls.Add(this.endLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Soccer";
+            this.Text = "Soccer";
+            this.Load += new System.EventHandler(this.Soccer_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Soccer_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label endLabel;
+    }
+}
+
---
+++ Soccer/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+namespace Soccer
+{
+    public partial class Soccer : Form
+    {
+        //Global Variables
+        Rectangle player1 = new Rectangle(10, 130, 20, 20);
+        Rectangle player2 = new Rectangle(330, 130, 20, 20);
+        Rectangle ball = new Rectangle(160, 100, 25, 25);
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        int playerSpeed = 3;
+        int ballXSpeed = 4;
+        int ballYSpeed = -4;
+
+        bool wPressed = false;
+        bool sPressed = false;
+        bool upArrowPressed = false;
+        bool downArrowPressed = false;
+
+        bool aPressed = false;
+        bool dPressed = false;
+        bool leftArrowPressed = false;
+        bool rightArrowPressed = false;
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+
+        SoundPlayer Goal = new SoundPlayer(Properties.Resources.cheer);
+        SoundPlayer Hit = new SoundPlayer(Properties.Resources.hit);
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.S:
+                    sPressed = true;
+                    break;
+                case Keys.Up:
+                    upArrowPressed = true;
+                    break;
+                case Keys.Down:
+                    downArrowPressed = true;
+                    break;
+                case Keys.A:
+                    aPressed = true;
+                    break;
+                case Keys.Left:
+                    leftArrowPressed = true;
+                    break;
+                case Keys.D:
+                    dPressed = true;
+                    break;
+                case Keys.Right:
+                    rightArrowPressed = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.S:
+                    sPressed = false;
+                    break;
+                case Keys.Up:
+                    upArrowPressed = false;
+                    break;
+                case Keys.Down:
+                    downArrowPressed = false;
+                    break;
+                case Keys.A:
+                    aPressed = false;
+                    break;
+                case Keys.Left:
+                    leftArrowPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
+                    break;
+                case Keys.Right:
+                    rightArrowPressed = false;
+                    break;
+            }
+
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            ball.X += ballXSpeed;
+            ball.Y += ballYSpeed;
+
+            if (wPressed == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+
+
+
+            if (aPressed == true && player1.X > 0)
+            {
+                player1.X -= playerSpeed;
+            }
+
+            if (dPressed == true && player1.X < this.Width - player1.Width)
+            {
+                player1.X += playerSpeed;
+            }
+
+
+
+
+            if (leftArrowPressed == true && player2.X > 0)
+            {
+                player2.X -= playerSpeed;
+            }
+
+            if (rightArrowPressed == true && player2.X < this.Width - player2.Width)
+            {
+                player2.X += playerSpeed;
+            }
+
+
+
+
+
+            if (sPressed == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            if (upArrowPressed == true && player2.Y > 0)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if (downArrowPressed == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed;
+            }
+
+
+
+
+
+
+
+
+
+
+
+            if (ball.Y < 0 || ball.Y > 280 - ball.Height)
+            {
+                ballYSpeed *= -1;
+            }
+
+            if (ball.X < 0 || ball.X > this.Width - ball.Width)
+            {
+                ballXSpeed *= -1;
+            }
+
+
+            if (player1.IntersectsWith(ball))
+
+            {
+                Rectangle topRec = new Rectangle(player1.X, player1.Y - 5, 20, 5);
+                Rectangle rightRec = new Rectangle(player1.X + 25, player1.Y, 5, 20);
+                Rectangle bottomRec = new Rectangle(player1.X, player1.Y + 25, 20, 5);
+                Rectangle leftRec = new Rectangle(player1.X - 5, player1.Y, 5, 20);
+
+                if (ball.IntersectsWith(topRec))
+                {
+                    ballYSpeed *= -1;
+                    ball.Y = player1.Y - ball.Height;
+
+                }
+
+                if (ball.IntersectsWith(rightRec))
+                {
+                    ballXSpeed *= -1;
+                    ball.X = player1.X + player1.Width;
+
+                }
+
+                if (ball.IntersectsWith(bottomRec))
+                {
+                    ballYSpeed *= -1;
+                    ball.Y = player1.Y + player1.Height;
+
+                }
+
+                if (ball.IntersectsWith(leftRec))
+                {
+                    ballXSpeed *= -1;
+                    ball.X = player1.X - ball.Width;
+
+                }
+
+                Hit.Play();
+
+
+            }
+
+            else if (player2.IntersectsWith(ball))
+            {
+                Rectangle topRec2 = new Rectangle(player2.X, player2.Y - 5, 20, 5);
+                Rectangle rightRec2 = new Rectangle(player2.X + 20, player2.Y, 5, 20);
+                Rectangle bottomRec2 = new Rectangle(player2.X, player2.Y + 25, 20, 5);
+                Rectangle leftRec2 = new Rectangle(player2.X - 5, player2.Y, 5, 20);
+
+                if (ball.IntersectsWith(topRec2))
+                {
+                    ballYSpeed *= -1;
+                    ball.Y = player2.Y - ball.Height;
+                }
+
+                if (ball.IntersectsWith(rightRec2))
+                {
+                    ballXSpeed *= -1;
+                    ball.X = player2.X + player2.Width;
+                }
+
+                if (ball.IntersectsWith(bottomRec2))
+                {
+                    ballYSpeed *= -1;
+                    ball.Y = player2.Y + player2.Height;
+                }
+
+                if (ball.IntersectsWith(leftRec2))
+                {
+                    ballXSpeed *= -1;
+                    ball.X = player2.X - ball.Width;
+                }
+
+                Hit.Play();
+                ;
+            }
+
+
+            if (ball.X < 0 && 180 > ball.Y && ball.Y > 100)
+            {
+                player2Score++;
+                p2ScoreLabel.Text = $"{player2Score}";
+
+                ball.X = 170;
+                ball.Y = 110;
+
+
+                player1.Y = 110;
+                player2.Y = 110;
+                player1.X = 10;
+                player2.X = 330;
+
+                Goal.Play();
+
+                Thread.Sleep(1000);
+               // Refresh();
+
+            }
+
+            else if (ball.X > 340 && 180 > ball.Y && ball.Y > 100)
+            {
+                player1Score++;
+                p1ScoreLabel.Text = $"{player1Score}";
+
+                ball.X = 120;
+                ball.Y = 110;
+
+                player1.Y = 110;
+                player2.Y = 110;
+                player1.X = 10;
+                player2.X = 330;
+
+                Goal.Play();
+
+                Thread.Sleep(1000);
+              //  Refresh();
+
+            }
+
+            if (player1Score == 3)
+            {
+                endLabel.Text = "Player 1 wins!";
+                gameTimer.Stop();
+            }
+
+            if (player2Score == 3)
+            {
+                endLabel.Text = "Player 2 wins!";
+                gameTimer.Stop();
+            }
+
+            if (ball.Y > this.Height)
+            {
+                ball.Y = this.Height - ball.Height;
+            }
+
+            if (ball.Y < 0)
+            {
+                ball.Y = 0;
+            }
+
+            if (ball.X > this.Width)
+            {
+                ball.X = this.Width - ball.Width;
+            }
+
+            if (ball.X < 0)
+            {
+                ball.X = 0 ;
+            }
+
+            Refresh();
+        }
+
+        public Soccer()
+        {
+            InitializeComponent();
+        }
+        private void Soccer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Soccer_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, player1);
+            e.Graphics.FillRectangle(whiteBrush, player2);
+            e.Graphics.FillEllipse(whiteBrush, ball);
+            e.Graphics.FillRectangle(whiteBrush, 180, 0, 15, this.Height);
+            e.Graphics.FillRectangle(whiteBrush, 0, 100, 10, 80);
+            e.Graphics.FillRectangle(whiteBrush, 362, 100, 10, 80);
+
+        }
+
+        private void p2ScoreLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
